5e1239a275a674ead4e4fa0730b8b1f327ad687f
Dani Sullivan <nz.danisullivan@gmail.com>
2024-10-08 16:13:54 +1300

    Extended OrderService
    Overridden methods

---
+++ src/services/order.ts
@@ -0,0 +1,71 @@
+import { FindConfig, OrderService as MedusaOrderService, Selector } from '@medusajs/medusa'
+import { Lifetime } from 'awilix'
+import { MedusaError } from 'medusa-core-utils'
+
+import type { User } from '../models/user'
+import type { Order } from '../models/order'
+
+type OrderSelector = {
+    store_id?: string
+} & Selector<Order>
+
+class OrderService extends MedusaOrderService {
+    static LIFE_TIME = Lifetime.TRANSIENT
+
+    protected readonly loggedInUser_: User | null
+
+    constructor(container, options) {
+        // @ts-ignore
+        super(...arguments)
+
+        try {
+            this.loggedInUser_ = container.loggedInUser
+        } catch (e) {
+            // avoid errors when backend first runs
+        }
+    }
+
+    async list(selector: OrderSelector, config?: FindConfig<Order>): Promise<Order[]> {
+        if (!selector.store_id && this.loggedInUser_?.store_id) {
+            selector.store_id = this.loggedInUser_.store_id
+        }
+
+        config.select?.push('store_id')
+
+        config.relations?.push('store')
+
+        return await super.list(selector, config)
+    }
+
+    async listAndCount(selector: OrderSelector, config?: FindConfig<Order>): Promise<[Order[], number]> {
+        if (!selector.store_id && this.loggedInUser_?.store_id) {
+            selector.store_id = this.loggedInUser_.store_id
+        }
+
+        config.select?.push('store_id')
+
+        config.relations?.push('store')
+
+        return await super.listAndCount(selector, config)
+    }
+
+    async retrieve(orderId: string, config: FindConfig<Order> = {}): Promise<Order> {
+
+        config.relations = [...(config.relations || []), 'store']
+
+        const order = await super.retrieve(orderId, config)
+
+        if (order.store?.id && 
+            this.loggedInUser_?.store_id && 
+            order.store.id !== this.loggedInUser_.store_id) {
+             throw new MedusaError(
+                MedusaError.Types.NOT_FOUND,
+                `Order with id ${orderId} was not found`
+            )
+        }
+
+        return order
+    }
+}
+
+export default OrderService
