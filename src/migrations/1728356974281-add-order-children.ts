802da5391693b96af568be9060dca42d825f0146
Dani Sullivan <nz.danisullivan@gmail.com>
2024-10-08 16:11:43 +1300

    Added Order children migrations

---
+++ src/migrations/1728356974281-add-order-children.ts
@@ -0,0 +1,15 @@
+import { MigrationInterface, QueryRunner } from "typeorm"
+
+export class AddOrderChildren1728356974281 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "order" ADD "order_parent_id" character varying`)
+        await queryRunner.query(`CREATE INDEX "OrderParentId" ON "order" ("order_parent_id")`)
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`DROP INDEX "public"."OrderParentId"`)
+        await queryRunner.query(`ALTER TABLE "order" DROP COLUMN "order_parent_id"`)
+    }
+
+}
