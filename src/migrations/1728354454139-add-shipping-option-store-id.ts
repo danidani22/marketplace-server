fa65d391f8b7f9519cb8e2cf1f6f33d119b46bb8
Dani Sullivan <nz.danisullivan@gmail.com>
2024-10-08 15:34:33 +1300

    Adjusted syntax

--- src/migrations/1728354454139-add-shipping-option-store-id.ts
+++ src/migrations/1728354454139-add-shipping-option-store-id.ts
@@ -3,9 +3,12 @@ import { MigrationInterface, QueryRunner } from "typeorm"
 export class AddShippingOptionStoreId1728354454139 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "shipping_option" ADD "store_id" character varying`)
+        await queryRunner.query(`CREATE INDEX "ShippingOptionStoreId" ON "shipping_option" ("store_id")`)
     }
-
+    
     public async down(queryRunner: QueryRunner): Promise<void> {
-    }
-
+        await queryRunner.query(`DROP INDEX "public"."ShippingOptionStoreId"`)
+        await queryRunner.query(`ALTER TABLE "shipping_option" DROP COLUMN "store_id"`)
+    }    
 }
