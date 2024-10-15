c2b2d9a7a4aeeec8d57dc37154916da7a7b203bf
Dani Sullivan <nz.danisullivan@gmail.com>
2024-10-15 22:08:52 +1300

    Installed search engine
    Updated config with search engine and stripe plugins

--- medusa-config.js
+++ medusa-config.js
@@ -1,37 +1,32 @@
-const dotenv = require("dotenv");
+const dotenv = require('dotenv');
 
-let ENV_FILE_NAME = "";
+let ENV_FILE_NAME = '';
 switch (process.env.NODE_ENV) {
-  case "production":
-    ENV_FILE_NAME = ".env.production";
+  case 'production':
+    ENV_FILE_NAME = '.env.production';
     break;
-  case "staging":
-    ENV_FILE_NAME = ".env.staging";
+  case 'staging':
+    ENV_FILE_NAME = '.env.staging';
     break;
-  case "test":
-    ENV_FILE_NAME = ".env.test";
+  case 'test':
+    ENV_FILE_NAME = '.env.test';
     break;
-  case "development":
+  case 'development':
   default:
-    ENV_FILE_NAME = ".env";
+    ENV_FILE_NAME = '.env';
     break;
 }
 
 try {
-  dotenv.config({ path: process.cwd() + "/" + ENV_FILE_NAME });
+  dotenv.config({ path: process.cwd() + '/' + ENV_FILE_NAME });
 } catch (e) {}
 
 // CORS when consuming Medusa from admin
-const ADMIN_CORS =
-  process.env.ADMIN_CORS || "http://localhost:7000,http://localhost:7001";
-
+const ADMIN_CORS = process.env.ADMIN_CORS;
 // CORS to avoid issues when consuming Medusa from a client
-const STORE_CORS = process.env.STORE_CORS || "http://localhost:8000";
-
-const DATABASE_URL =
-  process.env.DATABASE_URL || "postgres://localhost/medusa-starter-default";
-
-const REDIS_URL = process.env.REDIS_URL || "redis://localhost:6379";
+const STORE_CORS = process.env.STORE_CORS;
+const DATABASE_URL = process.env.DATABASE_URL;
+const REDIS_URL = process.env.REDIS_URL || 'redis://localhost:6379';
 
 const plugins = [
   `medusa-fulfillment-manual`,
@@ -39,17 +34,24 @@ const plugins = [
   {
     resolve: `@medusajs/file-local`,
     options: {
-      upload_dir: "uploads",
+      upload_dir: 'uploads',
     },
   },
   {
-    resolve: "@medusajs/admin",
+    resolve: '@medusajs/admin',
     /** @type {import('@medusajs/admin').PluginOptions} */
     options: {
       autoRebuild: true,
       develop: {
-        open: process.env.OPEN_BROWSER !== "false",
+        open: process.env.OPEN_BROWSER !== 'false',
       },
+    },
+  },
+  {
+    resolve: `medusa-payment-stripe`,
+    options: {
+      api_key: process.env.STRIPE_API_KEY,
+      capture: true, // For auto capturing the payments
     },
   },
 ];
@@ -71,11 +73,12 @@ const modules = {
 
 /** @type {import('@medusajs/medusa').ConfigModule["projectConfig"]} */
 const projectConfig = {
-  jwt_secret: process.env.JWT_SECRET || "supersecret",
-  cookie_secret: process.env.COOKIE_SECRET || "supersecret",
+  jwt_secret: process.env.JWT_SECRET || 'supersecret',
+  cookie_secret: process.env.COOKIE_SECRET || 'supersecret',
   store_cors: STORE_CORS,
   database_url: DATABASE_URL,
   admin_cors: ADMIN_CORS,
+  server_url: process.env.SERVER_URL || 'http://localhost:9000',
   // Uncomment the following lines to enable REDIS
   // redis_url: REDIS_URL
 };
