069293062109c9fafb770c5244fbb9bd4f3bbe4b
Dani Sullivan <nz.danisullivan@gmail.com>
2024-10-17 14:12:38 +1300

    Added store name route
    Custom API to pull the store information based on store name passed as a query

--- build/878.7d203d7e.chunk.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkmedusa_starter_default=self.webpackChunkmedusa_starter_default||[]).push([[878],{93878:(e,t,s)=>{s.r(t),s.d(t,{default:()=>a});let a=[{identifier:"local",extensions:[]}]}}]);
---
+++ src/api/store/name/route.ts
@@ -0,0 +1,24 @@
+import type { MedusaRequest, MedusaResponse } from '@medusajs/medusa';
+import { EntityManager } from 'typeorm';
+import { Store } from '../../../models/store';
+
+export const GET = async (req: MedusaRequest, res: MedusaResponse) => {
+  const storeName = req.query.store_name;
+
+  console.log(storeName as string)
+
+  if (!storeName) return res.json({ error: 'Missing store name field' });
+
+  const manager: EntityManager = req.scope.resolve<EntityManager>('manager');
+
+  await manager.transaction(async (manager) => {
+    const storeRepo = manager.getRepository(Store);
+    const store = await storeRepo.findOne({
+      where: { name: storeName as string },
+    });
+    if (!store) return res.json({ error: "Store by that name not found" })
+    
+    return res.json(store)
+  });
+
+};
