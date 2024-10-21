7972e75d15eccd8a3bd41733128eb825915d09ca
Dani Sullivan <nz.danisullivan@gmail.com>
2024-10-21 20:14:23 +1300

    Update products-from API

--- src/api/store/products-from/route.ts
+++ src/api/store/products-from/route.ts
@@ -5,21 +5,22 @@ import { EntityManager } from 'typeorm';
 import { Product } from '../../../models/product';
 
 export const GET = async (req: MedusaRequest, res: MedusaResponse) => {
-    const storeId = req.query.store_id;
-  
-  
-    if (!storeId) return res.json({ error: 'Missing store id field' });
-  
-    const manager: EntityManager = req.scope.resolve<EntityManager>('manager');
-  
-    await manager.transaction(async (manager) => {
-      const productRepo = manager.getRepository(Product);
-      const products = await productRepo.find({
-        where: { store_id: storeId as string },
-      });
-      if (!products) return res.json({ error: "Store by that name not founUnable to find any products" })
-      
-      return res.json({products: products})
+  const storeId = req.query.store_id;
+
+  if (!storeId) return res.json({ error: 'Missing store id field' });
+
+  const manager: EntityManager = req.scope.resolve<EntityManager>('manager');
+
+  await manager.transaction(async (manager) => {
+    const productRepo = manager.getRepository(Product);
+    const products = await productRepo.find({
+      where: { store_id: storeId as string },
     });
-  
-  };
+    if (!products)
+      return res.json({
+        error: 'Store by that name not founUnable to find any products',
+      });
+
+    return res.json({ products: products });
+  });
+};
