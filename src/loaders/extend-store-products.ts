25c69e23080a460af43c15ac165f356ce1335313
Dani Sullivan <nz.danisullivan@gmail.com>
2024-10-21 20:15:11 +1300

    Update Products Loader

--- src/loaders/extend-store-products.ts
+++ src/loaders/extend-store-products.ts
@@ -1,13 +1,29 @@
 // This loader ensures there is a product-store relation and adds the store fields.
 
 export default async function (): Promise<void> {
-    const module = await import('@medusajs/medusa/dist/api/routes/store/products/index')
+  const module = await import(
+    '@medusajs/medusa/dist/api/routes/store/products/index'
+  );
 
-    Object.assign(module, {
-        ...module,
-        defaultStoreProductsRelations: [...module.defaultStoreProductsRelations, 'store'],
-        defaultStoreProductsFields: [...module.defaultStoreProductsFields, 'store_id', 'featured'],
-        allowedStoreProductsRelations: [...module.allowedStoreProductsRelations, 'store'],
-        allowedStoreProductsFields: [...module.allowedStoreProductsFields, 'store_id', 'featured'],
-    })
+  Object.assign(module, {
+    ...module,
+    defaultStoreProductsRelations: [
+      ...module.defaultStoreProductsRelations,
+      'store',
+    ],
+    defaultStoreProductsFields: [
+      ...module.defaultStoreProductsFields,
+      'store_id',
+      'featured',
+    ],
+    allowedStoreProductsRelations: [
+      ...module.allowedStoreProductsRelations,
+      'store',
+    ],
+    allowedStoreProductsFields: [
+      ...module.allowedStoreProductsFields,
+      'store_id',
+      'featured',
+    ],
+  });
 }
